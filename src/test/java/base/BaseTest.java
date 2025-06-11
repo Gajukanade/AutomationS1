d293941b11281aed2553b6651544bc68e351243c
Gajanan <gajendrakanade77@gmail.com>
2025-06-11 15:46:08 +0530

    old code

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,5 @@
 package base;
 
-//package com.automation.base;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -21,24 +19,14 @@ public class BaseTest {
         // Setup ChromeDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
         
+        // Chrome options for better stability
         ChromeOptions options = new ChromeOptions();
-
-     // Headless for CI
-     options.addArguments("--headless=new");
-     options.addArguments("--no-sandbox");
-     options.addArguments("--disable-dev-shm-usage");
-     options.addArguments("--disable-extensions");
-     options.addArguments("--remote-allow-origins=*");
-     options.addArguments("--disable-gpu");
-
-     // 🔥 THIS LINE CAUSES THE ISSUE (REMOVE IT IF PRESENT)
-     // options.addArguments("--user-data-dir=...");  ❌
-
-     // Add ChromeDriver
-     WebDriver driver = new ChromeDriver(options);
-
-
-
+        options.addArguments("--disable-blink-features=AutomationControlled");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
@@ -54,3 +42,4 @@ public class BaseTest {
         }
     }
 }
+
